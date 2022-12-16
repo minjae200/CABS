e86574c8aba4f975aa1239ff41bd32160688f0b4
Minjae Choi <chlalswo200@naver.com>
2022-12-16 19:27:46 +0900

    modify template

--- CCC/static/js/traffic.js
+++ CCC/static/js/traffic.js
@@ -1,11 +1,10 @@
-const circles = document.querySelectorAll('.circle');
+const circles = document.querySelectorAll('.traffic-circle');
 let activeLight = 0;
 
 setInterval(changeLight, 1000);
 
 function changeLight() {
-//   1. remove init color
-  circles[activeLight].className = 'circle';  
+  circles[activeLight].className = 'traffic-circle';  
   activeLight++;
   
   if(activeLight > 2) {
@@ -14,5 +13,4 @@ function changeLight() {
   
   const currentLight = circles[activeLight];
   currentLight.classList.add(currentLight.getAttribute('color'));
-  //getAttribute로 사용자 정의 속성값을 가져올 수 있음
 }
