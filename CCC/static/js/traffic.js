58e4345f2e97d92eb9826ab89e69b367f2c7bb9d
Minjae Choi <chlalswo200@naver.com>
2022-12-25 11:05:13 +0900

    modify template

--- CCC/static/js/traffic.js
+++ CCC/static/js/traffic.js
@@ -1,16 +1,16 @@
-const circles = document.querySelectorAll('.traffic-circle');
-let activeLight = 0;
+// const circles = document.querySelectorAll('.traffic-circle');
+// let activeLight = 0;
 
-setInterval(changeLight, 1000);
+// setInterval(changeLight, 1000);
 
-function changeLight() {
-  circles[activeLight].className = 'traffic-circle';  
-  activeLight++;
+// function changeLight() {
+//   circles[activeLight].className = 'traffic-circle';  
+//   activeLight++;
   
-  if(activeLight > 2) {
-    activeLight = 0;
-  }
+//   if(activeLight > 2) {
+//     activeLight = 0;
+//   }
   
-  const currentLight = circles[activeLight];
-  currentLight.classList.add(currentLight.getAttribute('color'));
-}
+//   const currentLight = circles[activeLight];
+//   currentLight.classList.add(currentLight.getAttribute('color'));
+// }
